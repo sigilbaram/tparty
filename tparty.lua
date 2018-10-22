da0513a1dd6325e27bfd404eede6062aed3b3b4d
Sigil Baram <sigilbaram@gmail.com>
2018-10-22 17:40:42 -0400

    check that party is defined (i.e. chracter select)

--- tparty.lua
+++ tparty.lua
@@ -17,21 +17,25 @@ local config = settings.load(defaults)
 
 ui.display(function()
     local x_pos = windower.settings.ui_size.width - config.x_offset
-    for i=1,party[1].alliance_info.party_1_count do
-        local y_pos = windower.settings.ui_size.height -
-            config.party_y_offset -
-            config.member_y_spacing * (1 - i + party[1].alliance_info.party_1_count)
+    if party and party[1] then
+        for i=1,party[1].alliance_info.party_1_count do
+            local y_pos = windower.settings.ui_size.height -
+                config.party_y_offset -
+                config.member_y_spacing *
+                (1 - i + party[1].alliance_info.party_1_count)
 
-        local color = config.text_color
-        if party[i].tp>=1000 then
-            color = config.tp_full_color
+            local color = config.text_color
+            if party[i].tp>=1000 then
+                color = config.tp_full_color
+            end
+
+            ui.location(x_pos,y_pos)
+            ui.text(string.format(
+                '[%s]{8pt Arial bold italic stroke:"1px %s" color:%s}',
+                tostring(party[i].tp),
+                ui.color.tohex(config.stroke_color),
+                ui.color.tohex(color)
+            ))
         end
-
-        ui.location(x_pos,y_pos)
-        ui.text(string.format(
-            '[%s]{8pt Arial bold italic stroke:"1px %s" color:%s}',            tostring(party[i].tp),
-            ui.color.tohex(config.stroke_color),
-            ui.color.tohex(color)
-        ))
     end
 end)
