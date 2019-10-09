5fbd5e33bce46f8aa452f10fc146965cff34d938
Sigil Baram <sigilbaram@gmail.com>
2019-10-09 00:22:41 -0400

    Overhaul to use a window and improve settings

--- tparty.lua
+++ tparty.lua
@@ -5,37 +5,69 @@ local ui = require('ui')
 local windower = require('windower')
 
 local defaults = {
-    party_y_offset = 14,
-    x_offset = 120,
-    text_color = ui.color.rgb(255,255,255,185),
-    tp_full_color = ui.color.rgb(0,255,0,185),
-    stroke_color = ui.color.rgb(0,0,0,185),
-    member_y_spacing = 20
+    party = {
+        x_right = 16,
+        y_bottom = 16,
+        top_padding = 6,
+        total_y_padding = 14,
+        width = 112,
+        height_per_member = 20,
+        tp_x = 6,
+        tp_y = 9,
+    },
+    text = {
+        color = ui.color.rgb(255, 255, 255, 185),
+        tp_full_color = ui.color.rgb(0, 191, 255, 185),
+        stroke_color = ui.color.rgb(0, 0, 0, 185),
+        font = 'Arial',
+        party_font_size = '9pt',
+    },
+}
+
+local tparty_party_window_state = {
+    title = 'Tparty Party Window',
+    style = 'chromeless',
+    color = ui.color.transparent,
+    x = 0,
+    y = 0,
+    width = 112,
+    height = 134,
+    moveable = false,
+    closeable = false,
+    resizable = false,
 }
 
 local config = settings.load(defaults)
 
 ui.display(function()
-    local x_pos = windower.settings.ui_size.width - config.x_offset
-    if party and party[1] then
-        for i=1,party[1].alliance_info.party_1_count do
-            local y_pos = windower.settings.ui_size.height -
-                config.party_y_offset -
-                config.member_y_spacing *
-                (1 - i + party[1].alliance_info.party_1_count)
+    if party then
+        local party_1_count = party.alliance.party_1_count
+        tparty_party_window_state.x = windower.settings.ui_size.width - config.party.x_right - config.party.width
+        tparty_party_window_state.y = windower.settings.ui_size.height - config.party.y_bottom - (party_1_count * config.party.height_per_member) - config.party.total_y_padding
+        tparty_party_window_state.width = config.party.width
+        tparty_party_window_state.height = (party_1_count * config.party.height_per_member) + config.party.total_y_padding
 
-            local color = config.text_color
-            if party[i].tp>=1000 then
-                color = config.tp_full_color
+        tparty_party_window_state = ui.window('tparty_party_window', tparty_party_window_state, function()
+            for i=1, party_1_count do
+                local y = config.party.height_per_member * (i - 1) + config.party.top_padding
+                if i > 1 then
+                    y = y + 1
+                end
+
+                local color = config.text.color
+                if party[i].tp>=1000 then
+                    color = config.text.tp_full_color
+                end
+                ui.location(config.party.tp_x, y + config.party.tp_y)
+                ui.text(string.format(
+                    '[%s]{%s %s bold italic stroke:"1px %s" color:%s}',
+                    tostring(party[i].tp),
+                    config.text.party_font_size,
+                    config.text.font,
+                    ui.color.tohex(config.text.stroke_color),
+                    ui.color.tohex(color)
+                ))
             end
-
-            ui.location(x_pos,y_pos)
-            ui.text(string.format(
-                '[%s]{8pt Arial bold italic stroke:"1px %s" color:%s}',
-                tostring(party[i].tp),
-                ui.color.tohex(config.stroke_color),
-                ui.color.tohex(color)
-            ))
-        end
+        end)
     end
 end)
